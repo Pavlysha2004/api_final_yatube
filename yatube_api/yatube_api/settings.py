62d48bb8ef72e6430b736b704b4f41141f9e9441
Pavlysha2004 <xzkuropatka@gmail.com>
2025-05-21 03:12:22 +0300

    two

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -2,10 +2,6 @@ from pathlib import Path
 
 from datetime import timedelta
 
-import django.utils.timezone
-from datetime import timezone as dt_timezone
-django.utils.timezone.utc = dt_timezone.utc
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'hhz7l-ltdismtf@bzyz+rple7*s*w$jak%whj@(@u0eok^f9k4'
